647947d4cc0fb6d86b6fd8710d6817f27f65f5bf
droritos <drorgu102@gmail.com>
2025-03-28 20:16:02 +0300

    Semi Push

---
+++ Assets/Scripts/UI Things/MenuHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuHandler : MonoBehaviour
+{
+    [SerializeField] GameObject pauseFirstButton;
+    [SerializeField] GameObject optionsFirstButton;
+    [SerializeField] GameObject optionCloseButton;
+
+    public void OptionMenu()
+    {
+        
+    }
+}
