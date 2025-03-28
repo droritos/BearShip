49ee25c20f5e74d5fb26744b87b680a874e4173a
YonatanAmm <yonatan1922@gmail.com>
2025-03-28 18:50:16 +0300

    Sound Layout + Implementing few

--- Assets/Scripts/Artifact.cs
+++ Assets/Scripts/Artifact.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Artifact : MonoBehaviour
 {
+    [SerializeField] private List<AudioClip> collectSound;
+    
     public event UnityAction OnPickUpActionEvent;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) //Won't collide with enemies since neither have rigidbodies
     {
         OnPickUpActionEvent?.Invoke();
+        SoundManager.Instance.PlayRandomSfxSound(collectSound,transform);
         Destroy(this.gameObject);
     }
 }
