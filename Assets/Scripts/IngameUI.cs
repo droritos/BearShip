5d08f0b308322c68f6e956ba617ecfc3817d6578
FITZERed <157819329+FITZERed@users.noreply.github.com>
2025-03-28 20:17:08 +0300

    small organization and 3rd level

---
+++ Assets/Scripts/IngameUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class IngameUI : MonoBehaviour
+{
+    [Header("Win/Lose pop-ups")]
+    [SerializeField] private GameObject winPopup;
+    [SerializeField] private GameObject losePopup;
+
+    public void BackToMainMenu()
+    {
+        
+    }
+    public void BackToLastCheckpoint()
+    {
+        
+    }
+    
+}
