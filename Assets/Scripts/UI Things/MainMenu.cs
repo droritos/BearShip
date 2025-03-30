666faa58e2e16b2d509156a5768ccd53251dc3cd
droritos <drorgu102@gmail.com>
2025-03-30 08:39:23 +0300

    Fixed Menu start button

--- Assets/Scripts/UI Things/MainMenu.cs
+++ Assets/Scripts/UI Things/MainMenu.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject startButton;
     public void GoToLevel(int levelIndex = 1) // Main Menu Scene is index 0
     {
         SceneManager.LoadScene(levelIndex);
@@ -12,4 +15,20 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit Game");
         Application.Quit();
     }
+    private void Update()
+    {
+        EnsureButtonSelected();
+    }
+    private void EnsureButtonSelected()
+    {
+        if (startButton != null && EventSystem.current != null)
+        {
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                EventSystem.current.SetSelectedGameObject(startButton);
+                Debug.Log("startButton has been re-selected.");
+            }
+        }
+    }
+
 }
