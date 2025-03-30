baf69e0c12869a17d5f194e14b289b39ed60434b
droritos <drorgu102@gmail.com>
2025-03-30 06:27:57 +0300

    Done

--- Assets/Scripts/MixerController.cs
+++ Assets/Scripts/MixerController.cs
@@ -19,16 +19,6 @@ public class MixerController : MonoBehaviour
     public const string MasterVolume = "MA_Master";
     public const string FF = "F2";
 
-    private void Start()
-    {
-        valueText.text = "50%";
-        _currentVolume = 50f; // Set default to 50%
-
-        // Initialize the mixer with 50% volume (0.5 in slider value)
-        float initialSliderValue = 0.5f;
-        SetVolumeBasedOnType(initialSliderValue);
-    }
-
     public float GetAudioVolume()
     {
         float dbValue = 0f;
